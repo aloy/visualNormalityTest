5ccfd6c9f64dbfac394bf13137f8b3c3f7253de3
aloy <loyad01@gmail.com>
2014-11-07 08:35:26 -0600

    changed intention of simulation.R

--- R code/simulation.R
+++ R code/simulation.R
@@ -19,49 +19,7 @@
 library(lme4)     # for modeling
 library(HLMdiag)  # for residuals
 
-#-------------------------------------------------------------------------------
-# Functions
-#-------------------------------------------------------------------------------
-
-sim_random_intercept_model <- function(.mod, nsim, e.dsn, b0.dsn, sigma.err, sigma.b0){
-  vc <- VarCorr( .mod )
-  
-  n <- nobs(.mod)                   # No. obs.
-  nre <- unname( getME(.mod, "q") ) # No. ranefs
-  
-  ## Simulating error terms
-  if(e.dsn == "norm") {
-    e  <- rnorm(n = nsim * n, mean = 0, sd = sigma.err)
-  } 
-  if(e.dsn == "t") {
-    e  <- (sigma.err / sqrt(3)) * rt(n = nsim * n, df = 3)
-  }
-  if(e.dsn == "exp") {
-    e  <- sigma.err * ( rexp(n = nsim * n) - 1 )
-  }
-  e <- matrix(e, nc = nsim)
-  
-  ## Simulating random intercept
-  if(b0.dsn == "norm") {
-    b0  <- rnorm(n = nsim * nre, mean = 0, sd = sigma.b0)
-  } 
-  if(b0.dsn == "t") {
-    b0  <- (sigma.b0 / sqrt(3)) * rt(n = nsim * nre, df = 3)
-  }
-  if(b0.dsn == "exp") {
-    b0  <- sigma.b0 * ( rexp(n = nsim * nre) - 1 )
-  }
-  b0 <- matrix(b0, nc = nsim)
-  
-  ## Generating y
-  y <- getME(.mod, "X") %*% fixef(.mod) + getME(.mod, "Z") %*% b0 + e
-  
-  y_df <- as.data.frame( as.matrix(y) )
-  colnames(y_df) <- paste("sim_", 1:ncol(y_df), sep = "")
-  
-  return( y_df )
-}
-
+source("functions.R") 
 
 #-------------------------------------------------------------------------------
 # Simulation
@@ -71,12 +29,11 @@ sim_random_intercept_model <- function(.mod, nsim, e.dsn, b0.dsn, sigma.err, sig
 fm <- lmer(log.radon ~ basement + uranium + (1 | county), data = radon)
 
 # Trial simulations
-sim_random_intercept_model(.mod = fm, nsim = 1, e.dsn = "t", b0.dsn = "norm", 
-                           sigma.err = 2, sigma.b0 = 1)
+sim1 <- sim_random_intercept_model(.mod = fm, nsim = 1, e.dsn = "exp", 
+                                   b0.dsn = "t", sigma.err = 2, sigma.b0 = 1)
 
-sim_random_intercept_model(.mod = fm, nsim = 1, e.dsn = "norm", b0.dsn = "norm", 
-                           sigma.err = 2, sigma.b0 = 1)
+mod1 <- refit(sim1, object = fm)
+re1  <- std_ranef(mod1)
 
-sim_random_intercept_model(.mod = fm, nsim = 1, e.dsn = "exp", b0.dsn = "norm", 
-                           sigma.err = 2, sigma.b0 = 1)
-
+# Everything seems to be working based on my tests, and we don't hit the issue
+# with the standardized random effects using this set up.
